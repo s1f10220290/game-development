39dabdf49dbb3847a26c15cc6412295801ab24bd
s1f10220290 <107394006+s1f10220290@users.noreply.github.com>
2024-09-09 21:09:28 +0900

    画像の読み込み
    いつでも読み込めるように、もとになるコードをかいておきました！

---
+++ my_flask_app/image.py
@@ -0,0 +1,36 @@
+import pygame
+import sys
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("スパイミッションゲーム")
+    screen = pygame.display.set_mode((640, 360))
+    clock = pygame.time.Clock()
+    img_bg = pygame.image.load("pg_bg.png") #背景画像の読み込み
+    img_chara = [
+        pygame.image.load("pg_chara0.png"),
+        pygame.image.load("pg_chara1.png") #キャラ画像の読み込み
+    ]
+    tmr = 0
+
+    while True:
+        tmr = tmr + 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.event.KEYDOWN:  #キーを押すイベントが発生した時
+                if event.key == pygame.K_F1: #F1キーなら
+                    screen = pygame.display.set_mode((640,360),pygame.FULLSCREEN) #フルスクリーンにする
+                if event.key == pygame.K_F2 or event.key == pygame.K_ESCAPE:
+                    screen = pygame.display.set_mode((640,360))
+
+        x = tmr%160
+        for i in range(5):
+            screen.blit(img_bg, [i+160-x, 0])
+        screen.blit(img_chara[tmr%2], [224, 160])
+        pygame.display.update()
+        clock.tick(5)
+
+if __name__ == '__main__':
+    main()
